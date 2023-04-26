990ceb25adeda661eb78f3a6a9f353930e918055
Anas Jawed <103234658+anasjawed283@users.noreply.github.com>
2023-04-26 11:30:46 +0530

    about the course

---
+++ DAY1.R
@@ -0,0 +1,33 @@
+#introduction: Understanding Data types 
+#Generate Data
+1:10
+n=c(1,3,4,9)
+n
+c(1,'a',4,9)
+#Assign variable name to the value
+x=10; x<-10; x->10;
+x
+#To combine numeric values into a vector
+c(1,2,5)
+#Arithmetic Operations of vectors are 
+
+a+b
+d=c(1,2,3,4)
+
+
+d=c(3,4)
+e=c(5,6,7,8,9)
+d+e
+#subtraction
+a-b
+
+#character object is used to represent
+x=5.2
+x=as.character(5.2)#error is here because char and int are different data types
+x+5
+#concatenation of strings
+paste("hey", "there")
+#data importing
+#cslv, xls, xlsx, sav, dta, por, sas
+#importing data from Text and csv files
+read.csv(file.choose(file.csv))
